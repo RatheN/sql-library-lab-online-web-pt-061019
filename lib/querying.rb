0b6cce063aae221abf2ae42007770a757ffc8e8f
Nathan Rathe <nprathe10@gmail.com>
2019-08-20 05:42:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM characters
-  JOIN species
+  JOIN series
   ON series.id = characters.series_id
   GROUP BY series.title, characters.species
   HAVING characters.species = 'human'
